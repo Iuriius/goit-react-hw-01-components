1acf99d05a00a54fe2897ce81ea8dcc373d831cd
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-01-09 16:18:52 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
 import { Profile } from "./Profile/Profile.jsx";
-import user from "./Profile/user.json";
+import user from "./data/user.json";
 
 import { Statistics } from "./Statistics/Statistics.jsx";
-import data from "./Statistics/data.json";
+import data from "./data/data.json";
 
 import { FriendList } from "./FriendList/FriendList.jsx";
-import friends from "./FriendList/friends.json";
+import friends from "./data/friends.json";
 
 import { TransactionHistory } from "./Transaction/TransactionHistory.jsx";
-import transactions from "./Transaction/transactions.json";
+import transactions from "./data/transactions.json";
 
 export const App = () => {
   return (
